2b32a94067baf31d0fc3ae162fc6693614ec98d8
Goatella <contact@goatella.com>
2014-02-13 11:15:50 -0600

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,13 @@
+<?php 
+
+ $past = time() - 100; 
+
+ //this makes the time in the past to destroy the cookie 
+
+ setcookie(ID_my_site, gone, $past); 
+
+ setcookie(Key_my_site, gone, $past); 
+
+ header("Location: login.php"); 
+
+ ?> 
