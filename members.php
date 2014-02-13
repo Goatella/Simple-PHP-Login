c07f3180da63b20efc9c81c7b7794033d52094ad
Goatella <contact@goatella.com>
2014-02-13 11:16:22 -0600

    Create members.php

---
+++ members.php
@@ -0,0 +1,66 @@
+<?php 
+
+//Connects to your Database 
+ mysql_connect("YOUR.database.info.hostedresource.com", "YOURusername", "YOURpassword") or die(mysql_error()); 
+ mysql_select_db("YOURdatabase") or die(mysql_error()); 
+
+ 
+ //checks cookies to make sure they are logged in 
+
+ if(isset($_COOKIE['ID_my_site'])) 
+
+ { 
+
+ 	$username = $_COOKIE['ID_my_site']; 
+
+ 	$pass = $_COOKIE['Key_my_site']; 
+
+ 	 	$check = mysql_query("SELECT * FROM users WHERE username = '$username'")or die(mysql_error()); 
+
+ 	while($info = mysql_fetch_array( $check )) 	 
+
+ 		{ 
+
+ 
+
+ //if the cookie has the wrong password, they are taken to the login page 
+
+ 		if ($pass != $info['password']) 
+
+ 			{ 			header("Location: login.php"); 
+
+ 			} 
+
+ 
+
+ //otherwise they are shown the admin area	 
+
+ 	else 
+
+ 			{ 
+
+ 			 echo "Admin Area<p>"; 
+
+ echo "Your Content<p>"; 
+
+ echo "<a href=logout.php>Logout</a>"; 
+
+ 			} 
+
+ 		} 
+
+ 		} 
+
+ else 
+
+ 
+
+ //if the cookie does not exist, they are taken to the login screen 
+
+ {			 
+
+ header("Location: login.php"); 
+
+ } 
+
+ ?> 
